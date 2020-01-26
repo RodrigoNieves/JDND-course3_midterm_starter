adba2120cb3d46e04b734334621fd7edc654b534
Rodrigo Ruben Santiago Nieves <nievesnievesnieves@gmail.com>
2020-01-26 14:22:01 -0800

    Create Product Table

---
+++ P03-ReviewsAPI/src/main/resources/db/migration/V1__Create_tables.sql
@@ -0,0 +1,8 @@
+CREATE TABLE product (
+     product_id MEDIUMINT NOT NULL AUTO_INCREMENT,
+     title CHAR(255) NOT NULL,
+     description VARCHAR(1000) NOT NULL,
+     created_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+     stock INT NOT NULL DEFAULT 0,
+     PRIMARY KEY (product_id)
+);
