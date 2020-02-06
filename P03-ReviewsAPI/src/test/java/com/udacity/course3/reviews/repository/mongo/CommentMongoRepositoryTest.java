b8791f18fa817f13387845c78e12d7e565f251a9
Rodrigo Ruben Santiago Nieves <nievesnievesnieves@gmail.com>
2020-02-05 21:58:54 -0800

    Trying to fix

--- P03-ReviewsAPI/src/test/java/com/udacity/course3/reviews/repository/mongo/CommentMongoRepositoryTest.java
+++ P03-ReviewsAPI/src/test/java/com/udacity/course3/reviews/repository/mongo/CommentMongoRepositoryTest.java
@@ -1,36 +1,18 @@
 package com.udacity.course3.reviews.repository.mongo;
 
-import com.mongodb.DBCollection;
-import com.mongodb.Mongo;
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoCollection;
-import com.udacity.course3.reviews.entity.Comment;
 import com.udacity.course3.reviews.model.CommentMongo;
-import de.flapdoodle.embed.mongo.MongodExecutable;
-import de.flapdoodle.embed.mongo.MongodStarter;
-import de.flapdoodle.embed.mongo.config.IMongodConfig;
-import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
-import de.flapdoodle.embed.mongo.config.Net;
-import de.flapdoodle.embed.mongo.distribution.Version;
-import de.flapdoodle.embed.process.runtime.Network;
-import org.bson.Document;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @DataMongoTest
