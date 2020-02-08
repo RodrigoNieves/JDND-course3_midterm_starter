85de6307a5dea723f3cccfcc9d3adf2fa2581f02
Rodrigo Ruben Santiago Nieves <nievesnievesnieves@gmail.com>
2020-02-07 21:27:04 -0800

    Save comments

--- P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/persistance/PersistenceService.java
+++ P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/persistance/PersistenceService.java
@@ -58,7 +58,7 @@ public class PersistenceService {
         commentMongo.setDescription(savedComment.getDescription());
         commentMongo.setCreatedTime(savedComment.getCreatedTime());
         reviewMongo.getComments().add(commentMongo);
-
+        commentMongoRepository.save(commentMongo);
         reviewMongoRepository.save(reviewMongo);
         return Optional.of(savedComment);
     }
